1fa9f46700f5d5c3eae0f9c52421094103723afa
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-01-25 15:38:23 +1100

    injected js for forecast

--- src/app.js
+++ src/app.js
@@ -58,3 +58,32 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Melbourne");
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+          <div class="row">
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                width="40px"
+              />
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">25</span>
+                <span class="weather-forecast-temperature-min">17</span>
+              </div>
+            </div>
+          </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
